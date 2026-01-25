52763899edfee33368ed6e65abddb511a68eee1d
Codex2142 <sadatakbar04@gmail.com>
2026-01-25 14:02:18 +0700

    [PROGRESS] CRUD table (item)

--- app/Http/Controllers/Master/ItemController.php
+++ app/Http/Controllers/Master/ItemController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Master;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ItemResource;
 use App\Models\item;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -14,7 +15,14 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        // mengambil semua data berdasarkan terbaru
+        $item = item::latest()->get();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Data Berhasil Diambil!',
+            'data' => ItemResource::collection($item) // collection diguanakn ketika data berupa array
+        ], 200);
     }
 
     /**
@@ -23,11 +31,13 @@ class ItemController extends Controller
     public function store(Request $request)
     {
 
+        // validasi input $request sebelum diinsertkan
         $validator = Validator::make($request->all(),[
             'nama_item' => 'required|string|min:2|max:50',
             'harga' => 'required|integer'
         ]);
 
+        // respon json bila gagal validasi
         if($validator->fails()){
             return response()->json([
                 'status' => false,
@@ -36,12 +46,14 @@ class ItemController extends Controller
             ], 422);
         }
 
+        // menginsertkan item ke tabel database
         $item = item::create($request->all());
 
+        // respon bila data berhasil diinsertkan
         return response()->json([
             'status' => true,
             'message' => 'Item berhasil ditambahkan!',
-            'data' => $item
+            'data' => new ItemResource($item)
         ], 201);
     }
 
@@ -50,7 +62,23 @@ class ItemController extends Controller
      */
     public function show(string $id)
     {
-        //
+        // mencari item berdasarkan id yang cocok
+        $item = item::find($id);
+
+        // respon bila item tidak ditemukan
+        if(!$item){
+            return response()->json([
+                'status' => false,
+                'message' => 'Item tidak ditemukan!'
+            ], 404);
+        }
+
+        // respon bila berhasil ditemukan
+        return response()->json([
+            'status' => true,
+            'message' => 'Item ditemukan',
+            'data' => new ItemResource($item)
+        ], 200);
     }
 
     /**
@@ -58,7 +86,42 @@ class ItemController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        // mencari item berdasarkan id
+        $item = item::find($id);
+
+        // respon bila tidak ditemukan
+        if(!$item){
+            return response()->json([
+                'status' => false,
+                'message' => 'Item tidak ditemukan!'
+            ], 404);
+        }
+
+        // validasi input $request sebelum diupdate
+        $validator = Validator::make($request->all(),[
+            'nama_item' => 'required|string|min:2|max:50',
+            'harga' => 'required|integer'
+        ]);
+
+        // respon json bila gagal validasi
+        if($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'message' => 'Semua Form wajib diisi!',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $item->update($request->only([
+            'nama_item',
+            'harga'
+        ]));
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Item Berhasil Diupdate!',
+            'data' => new ItemResource($item)
+        ], 200);
     }
 
     /**
@@ -66,6 +129,22 @@ class ItemController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        // mencari item berdasarkan id
+        $item = item::find($id);
+
+        // respon bila tidak ditemukan
+        if(!$item){
+            return response()->json([
+                'status' => false,
+                'message' => 'Item tidak ditemukan!'
+            ], 404);
+        }
+
+        $item->delete();
+
+        return response()->json([
+            'status'=> true,
+            'message' => 'Item Berhasil DIhapus!',
+        ], 200);
     }
 }
